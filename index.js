f32f82471985197e1c26fd872a4eb12a4d9d8212
Arbash173 <arbashhumayun0@gmail.com>
2022-05-20 09:06:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Assignment 03
+
+// Q1
+// var a = prompt("Enter Value of a");
+// document.write("The Value of a is " +a)
+// document.write("<br>");
+
+// a++;
+// document.write("Now Value of a++ is " +a);
+// document.write("<br>");
+
+// ++a;
+// document.write("Now Value of a++ is " +a);
+// document.write("<br>");
+
+// a--;
+// document.write(" Now Value of a++ is " +a);
+// document.write("<br>");
+
+// --a;
+// document.write(" Now Value of a++ is " +a);
+// document.write("<br>");
+
+// Q2
+// var a = 2;
+// var b = 1;
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+// document.write("value of a " +a)
+// document.write("<br>");
+// document.write("value of a " +b)
+
+// Q3
+// var user = prompt("Enter Your name")
+// alert("Hello Amigo " +user)
+
+// Q4
+
+//  var num = parseInt(prompt('Enter an integer: '));
+    
+//  document.write("the table of "+num)
+// for(let i = 1; i <= 10; i++) {
+
+   
+//     const result = i * num;
+//     document.write("<br>");
+
+
+    
+    
+//     document.write(num +"*" +i +"=" +result )
+// }
+
+
